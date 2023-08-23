34dad33c730ac616ebf5381e8576b362a0cdc0ed
Sabbini-susritha <139756855+Sabbini-susritha@users.noreply.github.com>
2023-08-23 22:07:28 +0530

    mongodb connection

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import authRoutes from './routes/authRoutes.js';
+import dotenv from "dotenv";
+dotenv.config();
+import {} from "express-async-errors";
+import express from "express";
+import bodyParser from "body-parser";
+import authRoutes from "./routes/authRoutes.js";
+import connectDB from "./db/connectDB.js"
 
 const app = express();
 
@@ -8,9 +12,19 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
