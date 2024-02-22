cfba2c040e66d6fc6f28cd868748ffdefc739096
Sabbini-susritha <139756855+Sabbini-susritha@users.noreply.github.com>
2024-02-22 18:07:49 +0530

    RFS

--- server.js
+++ server.js
@@ -1,38 +1,46 @@
 import { PORT } from "./config/index.js";
 import {} from "express-async-errors";
 import express from "express";
-import cors  from 'cors';
+import cors from "cors";
 import bodyParser from "body-parser";
-import connectDB from "./db/connectDB.js"
+import connectDB from "./db/connectDB.js";
 import initRoutes from "./routes/index.js";
-import path from 'path';
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
+import path from "path";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import { connectToMongoDB } from "./db/mongoClient.js";
 import ErrorHandler from "./middleware/errorHandler.js";
+import helmet from "helmet";
+import rfs from "rotating-file-stream";
 
+const __filename = new URL(import.meta.url).pathname;
+// Use import.meta.url to derive __dirname
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
 
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d", // rotate daily
+  path: "./log",
+});
+
+app.use(morgan("combined", { stream: accessLogStream }));
+
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
-
-// Use import.meta.url to derive __dirname
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
+app.use(helmet());
 
 // Set up static files middleware
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api',initRoutes());
-
+app.use("/api", initRoutes());
 
 // ERROR HANDLER MIDDLEWARE (Last middleware to use)
-app.use(ErrorHandler)
+app.use(ErrorHandler);
 
 const port = PORT || 3000;
 
