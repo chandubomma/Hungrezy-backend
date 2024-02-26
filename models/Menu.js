cdc0fcba2162bb9f39f690aeb78118752c51cab1
Sabbini-susritha <139756855+Sabbini-susritha@users.noreply.github.com>
2024-02-26 22:41:28 +0530

    Version Key changed

--- models/Menu.js
+++ models/Menu.js
@@ -1,6 +1,6 @@
 import { Schema, model } from "mongoose";
 
-const MenuSchema = new Schema({}, { strict: false });
+const MenuSchema = new Schema({}, { strict: false, versionKey: false });
 
 const Menu = model("Menu", MenuSchema);
 
