380f31ce377a53daf0c26a8b23ab4c049cb535ea
Sabbini-susritha <139756855+Sabbini-susritha@users.noreply.github.com>
2024-03-03 10:42:41 +0530

    Customers

--- services/userService.js
+++ services/userService.js
@@ -1,39 +1,107 @@
 import { User } from "../models/index.js";
 import { dbUtils } from "../utils/index.js";
 import { authUtils } from "../utils/index.js";
-import * as Constants from '../constants/userRoleConstants.js'
+import * as Constants from "../constants/userRoleConstants.js";
 
-const TAG = 'service.user';
+const TAG = "service.user";
 
+const addImageDetails = async (data) => {
+  const { imageUrl, imageId, userId } = data;
+  if (!imageUrl && !imageId && userId)
+    throw {
+      message: "Image details not provided",
+      status: 400,
+    };
+  try {
+    const ObjectId = dbUtils.stringToObjectId(userId);
+    const user = await User.findById(ObjectId);
+    if (!user)
+      throw {
+        message: "User not found!",
+        status: 404,
+      };
+    user.image = imageUrl;
+    user.imageId = imageId;
+    await user.save();
+    const accessToken = await authUtils.generateAccessToken({
+      id: user.email,
+      user_role: Constants.USER_ROLE,
+      user,
+    });
+    const token = {};
+    token.accessToken = accessToken;
+    token.user = user;
+    return {
+      token,
+    };
+  } catch (error) {
+    console.error(`${TAG} ERROR in addImageDetails() => ${error}`);
+  }
+};
 
-const addImageDetails = async(data)=>{
-    const {imageUrl,imageId,userId} = data
-    if(!imageUrl && !imageId && userId)throw{
-        message : 'Image details not provided',
-        status : 400
-    }
-    try{
-        const ObjectId = dbUtils.stringToObjectId(userId)
-        const user = await User.findById(ObjectId)
-        if(!user)throw{
-            message : 'User not found!',
-            status : 404,
-        }
-        user.image = imageUrl;
-        user.imageId = imageId;
-        await user.save();
-        const accessToken = await authUtils.generateAccessToken({id:user.email,user_role:Constants.USER_ROLE,user});
-        const token = {}
-        token.accessToken = accessToken;
-        token.user = user;
-        return {
-            token
-        };
-    }catch(error){
-        console.error(`${TAG} ERROR in addImageDetails() => ${error}`);
-    }
-}
+const getAllUsers = async (data) => {
+  const { date, status } = data;
+  let query = {};
+  if (date) {
+    const dateQuery = dbUtils.getDateQuery(date);
+    query = { ...dateQuery };
+  }
+  if (status && status !== "all") {
+    query = { ...query, status: status };
+  }
 
-export {
-    addImageDetails,
-}
+  try {
+    const users = await User.find(query);
+    return {
+      status: 200,
+      message: "Users fetched successfully",
+      data: users,
+    };
+  } catch (error) {
+    console.error(`${TAG} ERROR in getAllUsers() => ${error}`);
+  }
+};
+
+const getUserDetails = async (id) => {
+  try {
+    const ObjectId = dbUtils.stringToObjectId(id);
+    const user = await User.findById(ObjectId);
+    if (!user)
+      throw {
+        message: "User not found!",
+        status: 404,
+      };
+    return {
+      status: 200,
+      message: "User fetched successfully",
+      data: user,
+    };
+  } catch (error) {
+    console.error(`${TAG} ERROR in getUserDetails() => ${error}`);
+  }
+};
+
+const updateCustomerStatus = async (req) => {
+  const { status } = req.body;
+  const id = req.params.id;
+  try {
+    const ObjectId = dbUtils.stringToObjectId(id);
+    const user = await User.findById(ObjectId);
+    if (!user)
+      throw {
+        message: "User not found!",
+        status: 404,
+      };
+    user.status = status;
+    await user.save();
+    return {
+      status: 200,
+      message: `User ${status} successfully!`,
+      data: user,
+    };
+  } catch (error) {
+    console.error(`${TAG} ERROR in updateCustomerStatus() => ${error}`);
+  }
+};
+
+export { addImageDetails, getAllUsers, getUserDetails, updateCustomerStatus };
